1a0a9e89497a4b05c88773d86e485c5988877655
fdufter <florian.dufter@tum.de>
2019-12-28 09:20:26 +0100

    Initial Commit

---
+++ GymBro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GymBro
+//
+//  Created by Florian Dufter on 28.12.19.
+//  Copyright © 2019 flocco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
